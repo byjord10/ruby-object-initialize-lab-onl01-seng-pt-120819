952f553ebeef875770399f13f5410b25bdd30842
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-09 16:46:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,6 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed= "Mutt")
     @name = name
     @breed = breed
   end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
 end
